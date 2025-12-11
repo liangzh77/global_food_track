bd9f80879b1f29af27df33664b8da9f1b2075630
liangzh77 <liangz77@qq.com>
2025-12-11 09:20:24 +0800

    删除web文件夹

--- web/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './styles/main.css'
-import 'flag-icons/css/flag-icons.min.css'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
